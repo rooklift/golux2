699daafc724720bb2e761e0a19211bbb1dedb104
rooklift <16438795+rooklift@users.noreply.github.com>
2023-01-25 21:24:14 +0000

    Update kit.go

--- kit/kit.go
+++ kit/kit.go
@@ -11,24 +11,20 @@ var scanner = bufio.NewScanner(os.Stdin)
 
 var gamestate GameState
 
-var logfile, _ = os.Create("log.txt")
-
 func Register() {
-	read_line_one()
-	fmt.Printf("{\"faction\": \"Golang\", \"bid\": 0}\n")
+	read_update()
+	fmt.Printf("{\"faction\": \"AlphaStrike\", \"bid\": 0}\n")
 	for {
 		read_update()
 		fmt.Printf("{}\n")
 	}
 }
 
-func read_line_one() {
+func read_update() {
 
 	var message Message1
 
 	scanner.Scan()
-
-	logfile.Write(scanner.Bytes())
 
 	err := json.Unmarshal(scanner.Bytes(), &message)
 	if err != nil {
@@ -36,27 +32,5 @@ func read_line_one() {
 	}
 
 	gamestate = message.GameState
-
-}
-
-func read_update() {
-
-	var message Message2			// Allegedly, the agent might actually receive the full gamestate, so this may be wrong.
-
-	scanner.Scan()
-
-	err := json.Unmarshal(scanner.Bytes(), &message)
-	if err != nil {
-		panic(fmt.Sprintf("%v", err))
-	}
-
-	update := message.GameStateUpdate
-
-	gamestate.EnvSteps = update.EnvSteps
-	gamestate.Units = update.Units
-	gamestate.Factories = update.Factories
-	gamestate.Teams = update.Teams
-
-	// TODO - parse update.Board
 
 }
