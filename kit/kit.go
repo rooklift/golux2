417b1f28c2f9a310ae62efc9d6546743f126f90c
rooklift <16438795+rooklift@users.noreply.github.com>
2023-01-25 23:08:36 +0000

    Update kit.go

--- kit/kit.go
+++ kit/kit.go
@@ -26,8 +26,8 @@ func Register(fn1 func(*Message), fn2 func(*Message), fn3 func(*Message)) {
 }
 
 func read_update() {					
-	var message Message								// Don't try to unmarshal into the already extant last_message since I'm not sure how that works
-	scanner.Scan()									// but I think it will allow old objects to persist, which we don't want...
+	var message Message								// Don't try to unmarshal into the already extant last_message since I'm not sure how that works -
+	scanner.Scan()									// in certain situations (maybe not this one) such an act can allow old objects to persist, don't want to worry.
 	logfile.Write(scanner.Bytes())
 	logfile.Write([]byte("\n"))
 	err := json.Unmarshal(scanner.Bytes(), &message)
