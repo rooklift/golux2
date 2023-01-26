955fd2126cef1c73ae233d460916b40f54a96db5
rooklift <16438795+rooklift@users.noreply.github.com>
2023-01-26 23:48:16 +0000

    TheirPlayerId, TheirFactories, TheirUnits

--- kit/info.go
+++ kit/info.go
@@ -6,8 +6,25 @@ func MyPlayerId() string {
 	return msg.Player
 }
 
+func TheirPlayerId() string {
+	_, ok := msg.Obs.Teams[MyPlayerId()]		// Sanity check that msg.Obs.Teams at least contains my id
+	if (!ok) {
+		panic("TheirPlayerId(): sanity check failed")
+	}
+	for key, _ := range msg.Obs.Teams {
+		if key != MyPlayerId() {
+			return key
+		}
+	}
+	panic("TheirPlayerId(): failed to get result")
+}
+
 func MyTeam() *Team {
 	return msg.Obs.Teams[MyPlayerId()]
+}
+
+func TheirTeam() *Team {
+	return msg.Obs.Teams[TheirPlayerId()]
 }
 
 func GetMsg() *Message {
@@ -30,30 +47,47 @@ func CanPlaceFactory() bool {
 	return (team.PlaceFirst && msg.Step % 2 == 1) || (!team.PlaceFirst && msg.Step % 2 == 0)
 }
 
-func MyFactories() []*Factory {						// We sort keys for deterministic order
-	my_fact_map := msg.Obs.Factories[MyPlayerId()]
+func get_factories(playerid string) []*Factory {					// We sort keys for deterministic order
+	fact_map := msg.Obs.Factories[playerid]
 	var keys []string
-	for key, _ := range my_fact_map {
+	for key, _ := range fact_map {
 		keys = append(keys, key)
 	}
 	sort.Strings(keys)
 	var ret []*Factory
 	for _, key := range keys {
-		ret = append(ret, my_fact_map[key])
+		ret = append(ret, fact_map[key])
 	}
 	return ret
 }
 
-func MyUnits() []*Unit {							// We sort keys for deterministic order
-	my_unit_map := msg.Obs.Units[MyPlayerId()]
+func MyFactories() []*Factory {	
+	return get_factories(MyPlayerId())
+}
+
+func TheirFactories() []*Factory {
+	return get_factories(TheirPlayerId())
+}
+
+func get_units(playerid string) []*Unit {							// We sort keys for deterministic order
+	unit_map := msg.Obs.Units[playerid]
 	var keys []string
-	for key, _ := range my_unit_map {
+	for key, _ := range unit_map {
 		keys = append(keys, key)
 	}
 	sort.Strings(keys)
 	var ret []*Unit
 	for _, key := range keys {
-		ret = append(ret, my_unit_map[key])
+		ret = append(ret, unit_map[key])
 	}
 	return ret
 }
+
+func MyUnits() []*Unit {
+	return get_units(MyPlayerId())
+}
+
+func TheirUnits() []*Unit {
+	return get_units(TheirPlayerId())
+}
+
