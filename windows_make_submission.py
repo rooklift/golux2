ee2796faec89ab30b7031488d88e4e4df471cf55
rooklift <16438795+rooklift@users.noreply.github.com>
2023-01-27 22:50:26 +0000

    use temp directory

--- windows_make_submission.py
+++ windows_make_submission.py
@@ -1,20 +1,17 @@
-import os, shutil, subprocess, tarfile
+import os, shutil, subprocess, tarfile, tempfile
 
 subprocess.run("go env -w GOOS=linux")
 subprocess.run("go build bot.go")
 subprocess.run("go env -w GOOS=windows")
 
-try:
-	os.mkdir("sub")
-except:
-	pass
-
-shutil.copy("bot", "sub/bot")
-shutil.copy("main.py", "sub/main.py")
-
 def set_permissions(tarinfo):
 	tarinfo.mode = 0o777
 	return tarinfo
 
-with tarfile.open("submission.tar.gz", "w:gz") as tar:
-	tar.add("sub", arcname="", filter=set_permissions)
+with tempfile.TemporaryDirectory() as tmpdirname:
+
+	shutil.move("bot", os.path.join(tmpdirname, "bot"))
+	shutil.copy("main.py", os.path.join(tmpdirname, "main.py"))
+
+	with tarfile.open("submission.tar.gz", "w:gz") as tar:
+		tar.add(tmpdirname, arcname="", filter=set_permissions)
