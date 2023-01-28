cc86287b8dffc489c62cffa75e7dbf3759f8db71
rooklift <16438795+rooklift@users.noreply.github.com>
2023-01-28 12:06:56 +0000

    rework

--- kit/info_cfg.go
+++ kit/info_cfg.go
@@ -1,36 +1,44 @@
 package kit
 
 import (
-	"fmt"
 	"math"
 )
 
-func (self *Frame) CfgInt(key string) (int, error) {
-
-	val, ok := self.Info.EnvCfg[key]
-	if ok == false {
-		return 0, fmt.Errorf("Key \"%s\" not in env_cfg dict")
+func (self *Frame) CfgFloat(key string) float64 {
+	if iface, ok1 := self.Info.EnvCfg[key]; ok1 {
+		if as_float, ok2 := iface.(float64); ok2 {
+			return as_float
+		}
 	}
-
-	switch n := val.(type) {
-	case float64:
-		return int(math.Round(n)), nil
-	default:
-		return 0, fmt.Errorf("Value was not a number")
-	}
+	return 0
 }
 
-func (self *Frame) CfgFloat(key string) (float64, error) {
+func (self *Frame) CfgInt(key string) int {
+	return int(math.Round(self.CfgFloat(key)))
+}
 
-	val, ok := self.Info.EnvCfg[key]
-	if ok == false {
-		return 0, fmt.Errorf("Key \"%s\" not in env_cfg dict")
+func (self *Frame) RobotCfgFloat(rtype int, key string) float64 {
+
+	var robotkey string
+	if rtype == HEAVY {
+		robotkey = "HEAVY"
+	} else if rtype == LIGHT {
+		robotkey = "LIGHT"
+	} else {
+		return 0
 	}
 
-	switch n := val.(type) {
-	case float64:
-		return n, nil
-	default:
-		return 0, fmt.Errorf("Value was not a number")
+	cfg_robots := self.Info.EnvCfg["ROBOTS"].(map[string]interface{})
+	cfg_this_weight := cfg_robots[robotkey].(map[string]interface{})
+
+	if iface, ok1 := cfg_this_weight[key]; ok1 {
+		if as_float, ok2 := iface.(float64); ok2 {
+			return as_float
+		}
 	}
+	return 0
+}
+
+func (self *Frame) RobotCfgInt(rtype int, key string) int {
+	return int(math.Round(self.RobotCfgFloat(rtype, key)))
 }
