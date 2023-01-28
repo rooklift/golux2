493ebc80b1d7154dde0760401e1508c122eadd88
rooklift <16438795+rooklift@users.noreply.github.com>
2023-01-28 01:01:49 +0000

    may as well just inline all this

--- kit/frame.go
+++ kit/frame.go
@@ -12,38 +12,36 @@ var decoder = json.NewDecoder(os.Stdin)
 // the thing being read is a whole {}-surrounded object. See https://github.com/golang/go/issues/3942
 
 func make_next_frame() *Frame {
+
 	var f *Frame						// Don't try to unmarshal into some already used object since I'm not sure how that works -
 	decoder.Decode(&f)					// the rules are complex and in many cases old stuff can persist; see the literature.
-	f.clear_actions()
-	f.fix_factory_occupancy()
-	f.fix_pointers()
-	return f
-}
 
-func (self *Frame) clear_actions() {
-	self.bid_string = "{}\n";
-	self.placement_string = "{}\n";
-	self.factory_actions = make(map[string]int)
-	self.unit_actions = make(map[string][][6]int)
-}
+	// Set the action variables to their defaults...
 
-func (self *Frame) fix_factory_occupancy() {
-	board := self.Obs.Board
-	board.FactoryOccupancy = make_2d_int_slice(self.Width(), self.Height(), -1)
-	for _, factory := range self.AllFactories() {
+	f.bid_string = "{}\n";
+	f.placement_string = "{}\n";
+	f.factory_actions = make(map[string]int)
+	f.unit_actions = make(map[string][][6]int)
+
+	// Fix the FactoryOccupancy map...
+
+	f.Obs.Board.FactoryOccupancy = make_2d_int_slice(f.Width(), f.Height(), -1)
+	for _, factory := range f.AllFactories() {
 		for x := factory.Pos[0] - 1; x <= factory.Pos[0] + 1; x++ {
 			for y := factory.Pos[1] - 1; y <= factory.Pos[1] + 1; y++ {
-				board.FactoryOccupancy[x][y] = factory.StrainId
+				f.Obs.Board.FactoryOccupancy[x][y] = factory.StrainId
 			}
 		}
 	}
-}
 
-func (self *Frame) fix_pointers() {
-	for _, unit := range self.AllUnits() {
-		unit.Frame = self
+	// Fix pointers in the units / factories...
+
+	for _, unit := range f.AllUnits() {
+		unit.Frame = f
 	}
-	for _, factory := range self.AllFactories() {
-		factory.Frame = self
+	for _, factory := range f.AllFactories() {
+		factory.Frame = f
 	}
+
+	return f
 }
