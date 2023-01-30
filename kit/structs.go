1ce619238b22bd6ddf7ae9fcdc674af09b224c36
rooklift <16438795+rooklift@users.noreply.github.com>
2023-01-30 16:29:30 +0000

    Comments

--- kit/structs.go
+++ kit/structs.go
@@ -20,8 +20,8 @@ type Info struct {
 type Obs struct {
 	RealEnvSteps			int								`json:"real_env_steps"`
 	Board					*Board							`json:"board"`			
-	Units					map[string]map[string]*Unit		`json:"units"`
-	Factories				map[string]map[string]*Factory	`json:"factories"`
+	Units					map[string]map[string]*Unit		`json:"units"`					// e.g. "player_0" --> "unit_10" --> *Unit
+	Factories				map[string]map[string]*Factory	`json:"factories"`				// e.g. "player_0" --> "factory_2" --> *Factory
 	Teams					map[string]*Team				`json:"teams"`
 }
 
@@ -32,15 +32,15 @@ type Board struct {
 	Lichen					[][]int							`json:"lichen"`
 	LichenStrains			[][]int							`json:"lichen_strains"`
 	ValidSpawnsMask			[][]bool						`json:"valid_spawns_mask"`
-	FactoryOccupancy		[][]int															// This is generated client side
+	FactoryOccupancy		[][]int															// Theoretically part of the Lux API - but generated client side
 	FactoriesPerTeam		int								`json:"factories_per_team"`
 }
 
 type Unit struct {
-	TeamId					int								`json:"team_id"`
-	UnitId					string							`json:"unit_id"`
+	TeamId					int								`json:"team_id"`				// 0 or 1
+	UnitId					string							`json:"unit_id"`				// e.g. "unit_10"
 	Power					int								`json:"power"`
-	UnitType				string							`json:"unit_type"`
+	UnitType				string							`json:"unit_type"`				// "LIGHT" or "HEAVY"
 	Pos						[2]int							`json:"pos"`
 	Cargo					*Cargo							`json:"cargo"`
 	ActionQueue				[][6]int						`json:"action_queue"`
@@ -49,12 +49,12 @@ type Unit struct {
 }
 
 type Factory struct {
-	TeamId					int								`json:"team_id"`
-	UnitId					string							`json:"unit_id"`
+	TeamId					int								`json:"team_id"`				// 0 or 1
+	UnitId					string							`json:"unit_id"`				// e.g. "factory_4"
 	Power					int								`json:"power"`
 	Pos						[2]int							`json:"pos"`
 	Cargo					*Cargo							`json:"cargo"`
-	StrainId				int								`json:"strain_id"`
+	StrainId				int								`json:"strain_id"`				// e.g. 4 - expected to match UnitId
 
 	Frame					*Frame							`json:"-"`
 }
@@ -69,8 +69,8 @@ type Cargo struct {
 type Team struct {
 	TeamId					int								`json:"team_id"`
 	Faction					string							`json:"faction"`
-	Water					int								`json:"water"`		// Not updated after factory placement phase!
-	Metal					int								`json:"metal"`		// Not updated after factory placement phase!
+	Water					int								`json:"water"`					// Not updated after factory placement phase!
+	Metal					int								`json:"metal"`					// Not updated after factory placement phase!
 	FactoriesToPlace		int								`json:"factories_to_place"`
 	FactoryStrains			[]int							`json:"factory_strains"`
 	PlaceFirst				bool							`json:"place_first"`
