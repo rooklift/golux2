94d3747ddb19292a4e21d2e8d2dc337c3bde1982
rooklift <16438795+rooklift@users.noreply.github.com>
2023-02-03 17:34:20 +0000

    more readable

--- kit/resource.go
+++ kit/resource.go
@@ -26,7 +26,6 @@ func (self *Frame) RubbleAt(xy XYer) int {
 
 func (self *Frame) AllIce() []Pos {
 	if self.cache.all_ice == nil {
-		self.cache.all_ice = []Pos{}			// Ensure not nil even if it ends up being empty
 		for x := 0; x < self.Width(); x++ {
 			for y := 0; y < self.Height(); y++ {
 				if self.IceAt(Pos{x, y}) {
@@ -35,6 +34,9 @@ func (self *Frame) AllIce() []Pos {
 			}
 		}
 	}
+	if self.cache.all_ice == nil {
+		self.cache.all_ice = []Pos{}
+	}
 	ret := make([]Pos, len(self.cache.all_ice))
 	copy(ret, self.cache.all_ice)
 	return ret
@@ -42,7 +44,6 @@ func (self *Frame) AllIce() []Pos {
 
 func (self *Frame) AllOre() []Pos {
 	if self.cache.all_ore == nil {
-		self.cache.all_ore = []Pos{}			// Ensure not nil even if it ends up being empty
 		for x := 0; x < self.Width(); x++ {
 			for y := 0; y < self.Height(); y++ {
 				if self.OreAt(Pos{x, y}) {
@@ -51,6 +52,9 @@ func (self *Frame) AllOre() []Pos {
 			}
 		}
 	}
+	if self.cache.all_ore == nil {
+		self.cache.all_ore = []Pos{}
+	}
 	ret := make([]Pos, len(self.cache.all_ore))
 	copy(ret, self.cache.all_ore)
 	return ret
@@ -58,7 +62,6 @@ func (self *Frame) AllOre() []Pos {
 
 func (self *Frame) AllRubble() []Pos {
 	if self.cache.all_rubble == nil {
-		self.cache.all_rubble = []Pos{}			// Ensure not nil even if it ends up being empty
 		for x := 0; x < self.Width(); x++ {
 			for y := 0; y < self.Height(); y++ {
 				if self.RubbleAt(Pos{x, y}) > 0 {
@@ -66,6 +69,9 @@ func (self *Frame) AllRubble() []Pos {
 				}
 			}
 		}
+	}
+	if self.cache.all_rubble == nil {
+		self.cache.all_rubble = []Pos{}
 	}
 	ret := make([]Pos, len(self.cache.all_rubble))
 	copy(ret, self.cache.all_rubble)
